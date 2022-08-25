d3c5920e3341c33adf944e2892de5d3ebbf3e805
Kaue Jacyntho <kauekaj@gmail.com>
2022-08-25 19:01:09 -0300

    Excluding Main Storyboard

--- FastFoodApp/LoginViewController.swift
+++ FastFoodApp/LoginViewController.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        view.backgroundColor = .red
     }
 
 

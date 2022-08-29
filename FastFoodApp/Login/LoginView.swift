b5118c3f2bf948aefddd1675df3f1cea4f7d98ba
Kaue Jacyntho <kauekaj@gmail.com>
2022-08-29 07:31:58 -0300

    Implementing email elements

--- FastFoodApp/Login/LoginView.swift
+++ FastFoodApp/Login/LoginView.swift
@@ -39,10 +39,16 @@ class LoginView: UIView {
         element.spacing = 5
         element.axis = .vertical
         element.isLayoutMarginsRelativeArrangement = true
-        element.layoutMargins = UIEdgeInsets(top: 6, left: 0, bottom: 6, right: 0)
+        element.layoutMargins = UIEdgeInsets(top:22, left: 0, bottom: 6, right: 0)
         return element
     }()
     
+    lazy var emailStackLine: UIView = {
+        let element = UIView()
+        element.translatesAutoresizingMaskIntoConstraints = false
+        element.backgroundColor = UIColor(red: 0.012, green: 0.02, blue: 0.016, alpha: 1)
+        return element
+    }()
     
     lazy var emailLabel: UILabel = {
         let element = UILabel()
@@ -56,7 +62,6 @@ class LoginView: UIView {
     lazy var emailTextField: UITextField = {
         let element = UITextField()
         element.translatesAutoresizingMaskIntoConstraints = false
-        element.backgroundColor = .red
         element.isHidden = true
         element.keyboardType = .emailAddress
         element.autocapitalizationType = .none
@@ -64,11 +69,18 @@ class LoginView: UIView {
         return element
     }()
     
+    
+    
+    
+    
+    
+    
     @objc
     func emailTapped() {
         emailLabel.font = UIFont.systemFont(ofSize: 11)
         emailTextField.isHidden = false
         emailStackView.layoutMargins = UIEdgeInsets(top: 6, left: 0, bottom: 6, right: 0)
+        emailStackLine.backgroundColor = UIColor(red: 1, green: 0.447, blue: 0.369, alpha: 1)
     }
     
     
@@ -116,6 +128,7 @@ extension LoginView: ViewCodable {
         addSubview(fastFoodApp)
         addSubview(emailImage)
         addSubview(emailStackView)
+        addSubview(emailStackLine)
     }
     
     func setupConstraints() {
@@ -133,8 +146,13 @@ extension LoginView: ViewCodable {
             
             emailStackView.bottomAnchor.constraint(equalTo: emailImage.bottomAnchor),
             emailStackView.leadingAnchor.constraint(equalTo: emailImage.trailingAnchor, constant: 14.4),
-            emailStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 38.2),
+            emailStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -38.2),
             emailStackView.heightAnchor.constraint(equalToConstant: 45),
+            
+            emailStackLine.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            emailStackLine.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+            emailStackLine.heightAnchor.constraint(equalToConstant: 1),
+            emailStackLine.bottomAnchor.constraint(equalTo: emailStackView.bottomAnchor),
             
             fastFoodApp.centerXAnchor.constraint(equalTo: centerXAnchor),
             fastFoodApp.centerYAnchor.constraint(equalTo: centerYAnchor)

c3351ab5289b93799fea470e4090be7850eb1200
Kaue Jacyntho <kauekaj@gmail.com>
2022-09-05 18:54:25 -0300

    Create LoginViewControllerSpy

---
+++ FastFoodAppTests/LoginViewControllerSpy.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewModelSpy.swift
+//  FastFoodAppTests
+//
+//  Created by Kaue de Assis Jacyntho on 05/09/22.
+//
+
+@testable import FastFoodApp
+
+//final class LoginViewControllerSpy: LoginViewDelegate {
+//
+//
+//    private{set} var isDidTapLogincalled = false
+//
+//    func didTapFacebookLogin() {
+//
+//    }
+//
+//    func didTapLogin() {
+//        isDidTapLogincalled = true
+//    }
+//    
+//}
